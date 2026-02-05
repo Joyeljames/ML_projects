6aa25c600dc32cf763372b9532f97b4fe26af5d8
Joyeljames <joyeljames333@gmail.com>
2026-02-05 22:55:22 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+hypen_e = "-e ."
+def get_requirements(file_path:str)->List[str]:
+
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if hypen_e in requirements:
+            requirements.remove(hypen_e)
+    
+    return requirements
+
+
+
+setup(
+name = "ML_Project",
+version = "0.1",
+author = "Joyel",
+author_email = "joyeljames333@gmail.com",
+packages = find_packages(),
+install_requires = get_requirements("requirements.txt")
+)
